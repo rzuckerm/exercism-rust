744084373d867386ed994d06c507e788507594e6
rzuckerm <ron.zuckerman@adtran.com>
2025-06-29 07:57:03 -0500

    Update Crypo Square in Rust

--- crypto-square/src/lib.rs
+++ crypto-square/src/lib.rs
@@ -4,8 +4,8 @@ pub fn encrypt(input: &str) -> String {
         .chars()
         .filter(char::is_ascii_alphanumeric)
         .collect::<Vec<char>>();
-    let r = 1.max((input.len() as f32).sqrt() as usize);
-    let c = (input.len() + r - 1) / r;
+    let r = 1.max((input.len() as f32).sqrt().round() as usize);
+    let c = input.len().div_ceil(r);
     (0..(r * c + 1.max(c) - 1))
         .map(|n| *input.get(n / (r + 1) + (n % (r + 1)) * c).unwrap_or(&' '))
         .collect()
